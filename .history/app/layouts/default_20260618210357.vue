c5515820fb8bb5b7b5b16b3817731cf24e340f9a
Lovethrech <rachealdolapo45@gmail.com>
2026-06-18 21:04:40 +0100

    ...clearing default nuxt template.

---
+++ .history/app/layouts/default_20260618210357.vue
@@ -0,0 +1,21 @@
+<script setup>
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+const router = useRouter()
+
+async function handleLogout() {
+    await supabase.auth.signOut()
+    router.push('/login')
+}
+</script>
+
+<template>
+  <div>
+    <nav>
+        <NuxtLink to="/">Home</NuxtLink>
+            <button v-if="user" @click="handleLogout">Log out</button>
+        <NuxtLink v-else to="/login">Log in</NuxtLink>
+    </nav>
+    <slot />
+  </div>
+</template>
