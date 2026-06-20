e66cbacecf757dc8071f6f62164f2995ce2bc667
Lovethrech <rachealdolapo45@gmail.com>
2026-06-20 13:39:02 +0100

    ...working on the auth page.

---
+++ .history/app/pages/login_20260620132927.vue
@@ -0,0 +1,51 @@
+<script setup>
+const title = 'Clout Collabs | Authentication'
+const description = ''
+
+useSeoMeta({
+    title,
+    description,
+    ogTitle: title,
+    ogDescription: description,
+    ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+    twitterCard: 'summary_large_image'
+})
+const authTitle=ref("Welcome back");
+const authDesc=ref("Log in to keep the conversation going.")
+</script>
+
+<template>
+    <div class="auth-login-page">
+        <AuthTitle :authTitle="authTitle"/>
+        <AuthDesc :authDesc="authDesc"/>
+        <form class="auth-login-page-form">
+            <AuthBtnGoogle/>
+            <div class="divider">
+                <span class="line"></span><span>or log in with email</span><span class="line"></span>
+            </div>
+            <AuthBtnFormEmailAndPassword/>
+            <Auth
+        </form>
+    </div>
+</template>
+
+<style scoped>
+.auth-login-page-form{
+    margin-top: 4vh;
+}
+.divider{ 
+    display:flex; 
+    align-items:center; 
+    gap:2vh; 
+    margin-bottom:2vh; 
+}
+.divider .line{ 
+    flex:1; 
+    height:0.1vh; 
+    background:var(--slate-700); 
+}
+.divider span{ 
+    font-size:1.4vh; 
+    color:var(--slate-400); 
+}
+</style>
