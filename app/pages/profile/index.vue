12f7c3428451f9dfb56b1b4c83500544c0e8b544
Lovethrech <rachealdolapo45@gmail.com>
2026-06-28 15:04:07 +0100

    Starting the profile page setup.

---
+++ app/pages/profile/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+const title = 'Dashboard | Clout Collabs'
+const description = ''
+
+useSeoMeta({
+    title,
+    description,
+    ogTitle: title,
+    ogDescription: description,
+    ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+    twitterCard: 'summary_large_image'
+})
+</script>
+
+<template>
+    <template>
+
+    </template>
+</template>
